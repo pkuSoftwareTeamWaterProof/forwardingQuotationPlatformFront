96a39997c8207c3645087e0a02e2265bcbaeab9a
Zejun <jelly.zhao.42@gmail.com>
2023-11-23 00:57:42 +0800

    dep: downgrade preline from 2.0.0 to 1.9.0

--- src/components/DashboardSidebar.tsx
+++ src/components/DashboardSidebar.tsx
@@ -1,6 +1,4 @@
 'use client'
-import { HSAccordion } from "preline";
-import PrelineLoader from "./PrelineLoader";
 
 export function DashboardSidebar() {
     return (
@@ -35,9 +33,6 @@ export function DashboardSidebar() {
               <button 
                 type="button" 
                 className="hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent text-sm text-slate-700 rounded-lg hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 dark:hs-accordion-active:text-white dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
-                onClick={() => {
-                    HSAccordion.show('#users-accordion-child');
-                }}
               >
               <svg className="flex-shrink-0 w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"/><path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"/></svg>
                 询价单
