a84137d2d1a285d3ba5de68f0e0b206a94dee9ce
Zejun <jelly.zhao.42@gmail.com>
2023-12-14 07:41:17 +0800

    feat: enable customers to make a deal

--- src/components/Requests.tsx
+++ src/components/Requests.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { apiURL, sheet } from "@/config"
+import { answer, apiURL, sheet } from "@/config"
 import { getCookie, getCookies } from "cookies-next"
 import { useEffect, useState } from "react"
 import { AnswerForm } from "./AnswerForm"
@@ -8,6 +8,7 @@ import { Princess_Sofia } from "next/font/google"
 export function Requests({ type }: { type: "firm" | "customer"}) {
   const [requests, setRequests] = useState([])
   const [curPrice, setCurPrice] = useState(0)
+  const [answers, setAnswers]: [answer[][], Function] = useState([])
   useEffect(() => {
     if(type === 'customer') {
       fetch(apiURL + '/api/sheet/list/' + getCookie('id'), {
@@ -31,6 +32,23 @@ export function Requests({ type }: { type: "firm" | "customer"}) {
         .then(data => setRequests(data))
     }
   }, [])
+
+  useEffect(() => {(async () => {
+    if(type === 'firm') return
+    const newAnswers: answer[][] = []
+    for (const request of requests) {
+      await fetch(apiURL + '/api/answer/sheet/' + request['id'], {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        headers: {
+          "accept": "*/*",
+          "Content-Type": "application/json",
+        },
+      }).then(res => res.json())
+        .then(data => newAnswers.push(data))
+    }
+    setAnswers(newAnswers)
+  })()}, [requests])
   
   return (
     <div className="flex flex-col">
@@ -48,12 +66,16 @@ export function Requests({ type }: { type: "firm" | "customer"}) {
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">体积</th>
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">起始时间</th>
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">终止时间</th>
-                  <th scope="col" className="px-6 py-3 text-end text-xs font-medium text-gray-500 uppercase">操作</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">最高报价</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">操作</th>
+                  {type === 'customer' && <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">操作</th>}
                 </tr>
               </thead>
               <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
                 {
-                  Array.from(requests).map((request: sheet) => (
+                  Array.from(requests).map((request: sheet) => {
+                    const answer = answers.filter(answer => (answer[0]? (answer[0].sheetID === request.id): false))[0]
+                    return (
                       <tr key={request.createdAt}>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.startpoint}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.endpoint}</td>
@@ -63,6 +85,7 @@ export function Requests({ type }: { type: "firm" | "customer"}) {
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.size}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.startdate}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.enddate}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{answer? answer.sort((a, b) => b.price - a.price)[0].price: 0}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
                           {type === 'firm' && <AnswerForm setCurPrice={setCurPrice} />}
                           <button 
@@ -108,8 +131,34 @@ export function Requests({ type }: { type: "firm" | "customer"}) {
                             {type==="firm"?"报价":"删除"}
                           </button>
                         </td>
+                        { type === 'customer' && 
+                        <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
+                        <button 
+                          type="button" 
+                          className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-600 hover:text-blue-800 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-400 dark:focus:outline-none dark:focus:ring-1 dark:focus:ring-gray-600"
+                          onClick={async (e) => {
+                            e.preventDefault()
+                            await fetch(apiURL + '/api/order/create', {
+                              method: "POST",
+                              mode: "cors",
+                              headers: {
+                                "accept": "*/*",
+                                "Content-Type": "application/json",
+                              },
+                              body: JSON.stringify({
+                                "sheetid": request.id,
+                                "answerid": answer[0].id,
+                                "context": "成交"
+                              })
+                            })
+                            alert("下单成功")
+                          }}
+                        >
+                          下单
+                        </button>
+                      </td>}
                       </tr>
-                    )
+                    )}
                   )
                 }
               </tbody>
