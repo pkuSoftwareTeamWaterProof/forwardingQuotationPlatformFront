8f9046081099eac3b7dd8f9e57e8e30aabd700e2
Zejun <jelly.zhao.42@gmail.com>
2023-12-14 08:13:34 +0800

    fix: fix bugs on displaying sheets

--- src/components/Requests.tsx
+++ src/components/Requests.tsx
@@ -66,7 +66,7 @@ export function Requests({ type }: { type: "firm" | "customer"}) {
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">体积</th>
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">起始时间</th>
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">终止时间</th>
-                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">最高报价</th>
+                  <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">最低报价</th>
                   <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">操作</th>
                   {type === 'customer' && <th scope="col" className="px-6 py-3 text-start text-xs font-medium text-gray-500 uppercase">操作</th>}
                 </tr>
@@ -85,7 +85,7 @@ export function Requests({ type }: { type: "firm" | "customer"}) {
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.size}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.startdate}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{request.enddate}</td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{answer? answer.sort((a, b) => b.price - a.price)[0].price: 0}</td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-gray-200">{answer? answer.sort((a, b) => a.price - b.price)[0].price: 0}</td>
                         <td className="px-6 py-4 whitespace-nowrap text-end text-sm font-medium">
                           {type === 'firm' && <AnswerForm setCurPrice={setCurPrice} />}
                           <button 
